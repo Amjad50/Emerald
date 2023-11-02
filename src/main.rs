94c34b7e4940abd394cccd946975946c14c2133f
Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>
2023-11-02 12:02:17 +0800

    Kernel/Boot: Boot to rust (64bit)
    Here, we setup all the needed stuff in order to move to rust safely in
    64bit environment.
    
    This includes:
    - Setting up virtual memory.
        We are mapping the ranges 0x0000000000000000..0x0000000007FFFFFF
        and 0xFFFFFFFF80000000..0xFFFFFFFF87FFFFFF to the same range. This
        is equal to 128MB (not sure what will happen if the ram is less than
        that XD).
    - preparing all the needed flags and GDT, to move to Long mode (64bit)
    - Jump to a near location using relative jump and enter 64bit mode.
    - Setup the stack and clear the segments (not needed in 64bit mode)
    - Pass the `MultiBoot Info` ptr to the kernel, where we will start
      processing and so on.
    - Now we have access to 64bit addresses, so we can load the full address
      of the `kernel_main` and jump to it.
    
    And done, took me some time to debug some issues with page tables, but
    now its working.

--- src/main.rs
+++ src/main.rs
@@ -6,60 +6,23 @@ pub static KERNEL_BASE: u64 = 0xFFFFFFFF80000000;
 pub static TEXT_OFFSET: u64 = 0x100000;
 pub static KERNEL_LINK: u64 = KERNEL_BASE + TEXT_OFFSET;
 
-core::arch::global_asm!("
-    .section .multiboot_header
-    .set mb_magic, 0x1BADB002
-    .set mb_flags, 0x10000
-    .set mb_checksum, -mb_magic-mb_flags
+core::arch::global_asm!(include_str!("boot.S"));
 
-    .long mb_magic
-    .long mb_flags
-    .long mb_checksum
-    .long multiboot_load_addr
-    .long multiboot_load_addr
-    .long multiboot_load_end
-    .long multiboot_bss_end
-    .long multiboot_entry_addr
+macro_rules! pause {
+    () => {
+        unsafe {
+            core::arch::asm!("pause");
+        }
+    };
+}
 
-    # a helper macro that converts a virtual address to a physical address
-    # this should be used when loading any address while paging is disabled
-    .macro virtual_to_physical reg:req,  addr:req
-        mov \\reg, offset \\addr - 0xFFFFFFFF80000000
-    .endm
-
-    .section .text
-    .code32
-    .global entry
-    entry:
-        cmp eax, 0x2BADB002
-        jne error_halt
-
-    # load the kernel
-    tmp_loop:
-        pause
-        jmp tmp_loop
-
-    error_halt:
-        mov edi, 0xb8000
-        virtual_to_physical esi, message
-    print:
-        mov al, [esi]
-        cmp al, 0
-        je halt_loop
-        mov [edi], al
-        mov byte ptr [edi+1], 12    # red on black
-        add edi, 2
-        inc esi
-        jmp print
-
-    halt_loop:
-        pause
-        jmp halt_loop
-
-    .section .rodata
-    message:
-        .ascii  \"[ERROR] Not a valid multiboot result!!!\0\"
-");
+#[link_section = ".text"]
+#[no_mangle]
+pub extern "C" fn kernel_main(_multiboot_info_ptr: u64) -> ! {
+    loop {
+        pause!();
+    }
+}
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
