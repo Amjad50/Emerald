8f7ef20d58de78320e4f59ad76e8e9fa15bce3ca
Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>
2023-11-06 08:59:31 +0800

    Kernel/IDT: fixed issues with infinite loop interrupts
    Turned out I was not using the TSS stacks, since I was using entry `0`, which means,
    keep the same execution stack.
    
    So, I fixed the API, so that we can specify that we are adding a special stack index.
    
    I applied this already to default handlers that deal with faults,
    such as page-fault and double-fault.
    
    This fixed the issues where stackoverflow crashes qemu. it was triggering an exception
    while trying to handle the page-fault exception, but using the same stack.
    
    I'm still having the issue where `set_interrupts` trigger double-fault without error-code (the enteries in the stack are not correct).

--- src/cpu/idt.rs
+++ src/cpu/idt.rs
@@ -1,4 +1,4 @@
-use core::{hint, marker::PhantomData, mem};
+use core::{marker::PhantomData, mem};
 
 pub type InterruptHandler = extern "x86-interrupt" fn(frame: InterruptStackFrame64);
 pub type InterruptHandlerWithError =
@@ -47,42 +47,46 @@ impl<T> InterruptDescriptorTableEntry<T> {
         }
     }
 
-    fn set_handler_ptr(&mut self, handler_addr: u64, stack_index: u8, disable_interrupts: bool) {
-        assert!(stack_index < 7);
+    fn set_handler_ptr(&mut self, handler_addr: u64) -> &mut Self {
         let cs = super::get_cs();
         self.offset_low = handler_addr as u16;
         self.offset_middle = (handler_addr >> 16) as u16;
         self.offset_high = (handler_addr >> 32) as u32;
-        self.ist = stack_index;
+        self.ist = 0;
         self.selector = cs;
         self.flags = flags::PRESENT | flags::GATE_TYPE;
-        if !disable_interrupts {
+        self
+    }
+
+    pub fn set_stack_index(&mut self, stack_index: Option<u8>) -> &mut Self {
+        let stack_index = stack_index.map(|i| i + 1).unwrap_or(0);
+        assert!(stack_index <= 7);
+        self.ist = stack_index;
+        self
+    }
+
+    #[allow(dead_code)]
+    pub fn set_disable_interrupts(&mut self, disable_interrupts: bool) -> &mut Self {
+        if disable_interrupts {
+            self.flags &= !flags::KEEP_INTERRUPTS;
+        } else {
             self.flags |= flags::KEEP_INTERRUPTS;
         }
+        self
     }
 }
 
 #[allow(dead_code)]
 impl InterruptDescriptorTableEntry<InterruptHandler> {
-    pub fn set_handler(
-        &mut self,
-        handler: InterruptHandler,
-        stack_index: u8,
-        disable_interrupts: bool,
-    ) {
-        self.set_handler_ptr(handler as *const u8 as u64, stack_index, disable_interrupts);
+    pub fn set_handler(&mut self, handler: InterruptHandler) -> &mut Self {
+        self.set_handler_ptr(handler as *const u8 as u64)
     }
 }
 
 #[allow(dead_code)]
 impl InterruptDescriptorTableEntry<InterruptHandlerWithError> {
-    pub fn set_handler(
-        &mut self,
-        handler: InterruptHandlerWithError,
-        stack_index: u8,
-        disable_interrupts: bool,
-    ) {
-        self.set_handler_ptr(handler as *const u8 as u64, stack_index, disable_interrupts);
+    pub fn set_handler(&mut self, handler: InterruptHandlerWithError) -> &mut Self {
+        self.set_handler_ptr(handler as *const u8 as u64)
     }
 }
 
@@ -153,50 +157,49 @@ impl InterruptDescriptorTable {
     }
 
     pub fn init_default_handlers(&mut self) {
-        self.divide_by_zero
-            .set_handler(default_handler::<0>, 0, true);
-        self.debug.set_handler(default_handler::<1>, 0, true);
+        self.divide_by_zero.set_handler(default_handler::<0>);
+        self.debug.set_handler(default_handler::<1>);
         self.non_maskable_interrupt
-            .set_handler(default_handler::<2>, 0, true);
-        self.breakpoint.set_handler(default_handler::<3>, 0, true);
-        self.overflow.set_handler(default_handler::<4>, 0, true);
+            .set_handler(default_handler::<2>);
+        self.breakpoint.set_handler(default_handler::<3>);
+        self.overflow.set_handler(default_handler::<4>);
         self.bound_range_exceeded
-            .set_handler(default_handler::<5>, 0, true);
+            .set_handler(default_handler::<5>)
+            .set_stack_index(Some(1));
         self.invalid_opcode
-            .set_handler(default_handler::<6>, 0, true);
+            .set_handler(default_handler::<6>)
+            .set_stack_index(Some(1));
         self.device_not_available
-            .set_handler(default_handler::<7>, 0, true);
+            .set_handler(default_handler::<7>)
+            .set_stack_index(Some(1));
         self.double_fault
-            .set_handler(default_handler_with_error::<8>, 0, true);
+            .set_handler(default_handler_with_error::<8>)
+            .set_stack_index(Some(2));
         self.invalid_tss
-            .set_handler(default_handler_with_error::<10>, 0, true);
+            .set_handler(default_handler_with_error::<10>);
         self.segment_not_present
-            .set_handler(default_handler_with_error::<11>, 0, true);
+            .set_handler(default_handler_with_error::<11>);
         self.stack_exception
-            .set_handler(default_handler_with_error::<12>, 0, true);
+            .set_handler(default_handler_with_error::<12>);
         self.general_protection_fault
-            .set_handler(default_handler_with_error::<13>, 0, true);
+            .set_handler(default_handler_with_error::<13>);
         self.page_fault
-            .set_handler(default_handler_with_error::<14>, 0, true);
-        self.x87_floating_point
-            .set_handler(default_handler::<16>, 0, true);
+            .set_handler(default_handler_with_error::<14>)
+            .set_stack_index(Some(3));
+        self.x87_floating_point.set_handler(default_handler::<16>);
         self.alignment_check
-            .set_handler(default_handler_with_error::<17>, 0, true);
-        self.machine_check
-            .set_handler(default_handler::<18>, 0, true);
-        self.simd_floating_point
-            .set_handler(default_handler::<19>, 0, true);
-        self.control_protection
-            .set_handler(default_handler::<21>, 0, true);
-        self.hypervisor_injection
-            .set_handler(default_handler::<28>, 0, true);
-        self.vmm_communication
-            .set_handler(default_handler::<29>, 0, true);
+            .set_handler(default_handler_with_error::<17>)
+            .set_stack_index(Some(2));
+        self.machine_check.set_handler(default_handler::<18>);
+        self.simd_floating_point.set_handler(default_handler::<19>);
+        self.control_protection.set_handler(default_handler::<21>);
+        self.hypervisor_injection.set_handler(default_handler::<28>);
+        self.vmm_communication.set_handler(default_handler::<29>);
         self.security_exception
-            .set_handler(default_handler_with_error::<30>, 0, true);
+            .set_handler(default_handler_with_error::<30>);
 
         for entry in self.user_defined.iter_mut() {
-            entry.set_handler(default_handler::<0xFF>, 0, true);
+            entry.set_handler(default_handler::<0xFF>);
         }
     }
 
@@ -219,23 +222,19 @@ pub(super) struct InterruptDescriptorTablePointer {
 }
 
 extern "x86-interrupt" fn default_handler<const N: u8>(frame: InterruptStackFrame64) {
-    println!("[{N}] Got exception: \n frame: {:x?}", frame);
-
-    loop {
-        hint::spin_loop();
-    }
+    panic!("[{N}] Got exception: \n frame: {:x?}", frame);
 }
 
 extern "x86-interrupt" fn default_handler_with_error<const N: u8>(
     frame: InterruptStackFrame64,
     error_code: u64,
 ) {
-    println!(
-        "[{N}] Got exception: \n frame: {:x?}\n error: {:016X}",
-        frame, error_code
-    );
-
-    loop {
-        hint::spin_loop();
+    let cr2: u64;
+    unsafe {
+        core::arch:: asm!("mov {}, cr2", out(reg) cr2);
     }
+    panic!(
+        "[{N}] Got exception: \n frame: {:x?}\n error: {:016X}\n cr2: {:X}",
+        frame, error_code, cr2
+    );
 }
