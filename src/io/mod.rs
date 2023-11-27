3a05ff15c1c8af2597aba9729b8ed2ac3a5c0f8c
Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>
2023-11-27 11:10:02 +0800

    Debug: added `NoDebug` to disable debug for unneeded fields, and `hexdump`
    In large structures with large arrays, we don't want to print them, since sometimes they are just extra junk.
    That's why we added `NoDebug` wrapper.
    
    `hexdump` is also a useful tool to debug memory buffers easily.

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,4 +1,4 @@
-use core::sync::atomic::AtomicBool;
+use core::{fmt, ops, sync::atomic::AtomicBool};
 
 pub mod console;
 mod keyboard;
@@ -6,6 +6,87 @@ mod uart;
 mod video_memory;
 
 static PRINT_ERR: AtomicBool = AtomicBool::new(false);
+
+// This is a wrapper around a type that implements Debug, but we don't want to print it
+// its kinda like using some libraries that allow you to disable debug for some fields
+#[repr(transparent)]
+pub struct NoDebug<T>(pub T);
+
+impl<T> fmt::Debug for NoDebug<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "[no_debug]")
+    }
+}
+
+impl<T> Clone for NoDebug<T>
+where
+    T: Clone,
+{
+    fn clone(&self) -> Self {
+        NoDebug(self.0.clone())
+    }
+}
+impl<T> Copy for NoDebug<T> where T: Copy {}
+
+impl<T> ops::Deref for NoDebug<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T> ops::DerefMut for NoDebug<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+#[allow(dead_code)]
+pub fn hexdump(buf: &[u8]) {
+    // lock first so that none else can access the console
+    // its ReMutex so we can aquire the lock
+    let _con = console::CONSOLE.lock();
+
+    // print hex dump
+    for i in 0..buf.len() / 16 {
+        print!("{:08X}:  ", i * 16);
+        for j in 0..16 {
+            print!("{:02X} ", buf[i * 16 + j]);
+        }
+        // print ascii
+        print!("  ");
+        for j in 0..16 {
+            let c = buf[i * 16 + j];
+            if (32..127).contains(&c) {
+                print!("{}", c as char);
+            } else {
+                print!(".");
+            }
+        }
+        println!();
+    }
+    // print remaining if any
+    let remaining = buf.len() % 16;
+    if remaining != 0 {
+        let remaining_start = (buf.len() / 16) * 16;
+
+        print!("{:08X}:  ", remaining_start);
+        for c in buf[remaining_start..].iter() {
+            print!("{:02X} ", c);
+        }
+        // print ascii
+        print!("  ");
+        for &c in buf[remaining_start..].iter() {
+            if (32..127).contains(&c) {
+                print!("{}", c as char);
+            } else {
+                print!(".");
+            }
+        }
+        println!();
+    }
+}
 
 pub fn _print(args: ::core::fmt::Arguments) {
     use core::fmt::Write;
