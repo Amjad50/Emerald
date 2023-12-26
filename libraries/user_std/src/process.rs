7db7735d6de2176f658cc68aeca12d775371c8d0
Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>
2023-12-26 17:20:06 +0800

    User+Lib+Extern: added processes spawning in `std`
    Now, we only use `std` fully in `init` and `shell`.

--- libraries/user_std/src/process.rs
+++ libraries/user_std/src/process.rs
@@ -1,4 +1,9 @@
-use kernel_user_link::{call_syscall, syscalls::SYS_EXIT};
+use core::ffi::{c_char, CStr};
+
+use kernel_user_link::{
+    call_syscall,
+    syscalls::{SyscallError, SYS_EXIT, SYS_SPAWN},
+};
 
 /// # Safety
 /// No guarantees are made about the state of the system after this function returns.
@@ -11,4 +16,17 @@ pub unsafe fn exit(code: u64) -> ! {
         .unwrap();
     }
     unreachable!("exit syscall should not return")
+}
+
+/// # Safety
+/// path must be a valid C string.
+/// argv must be a valid C string array. ending with a null pointer.
+pub unsafe fn spawn(path: &CStr, argv: &[*const c_char]) -> Result<u64, SyscallError> {
+    unsafe {
+        call_syscall!(
+            SYS_SPAWN,
+            path.as_ptr() as u64, // path
+            argv.as_ptr() as u64, // argv
+        )
+    }
 }
--- userspace/init/src/main.rs
+++ userspace/init/src/main.rs
@@ -1,27 +1,10 @@
 #![feature(restricted_std)]
 
-use core::ffi::{c_char, CStr};
-
-use kernel_user_link::{call_syscall, syscalls::SYS_SPAWN};
-
-fn spawn(path: &CStr, argv: &[*const c_char]) -> u64 {
-    unsafe {
-        call_syscall!(
-            SYS_SPAWN,
-            path.as_ptr() as u64, // path
-            argv.as_ptr() as u64, // argv
-        )
-        .unwrap()
-    }
-}
-
 fn main() {
     // we are in `init` now
     println!("[init] Hello!\n\n");
 
-    let shell_path = c"/shell";
-    let shell_argv = [shell_path.as_ptr(), c"".as_ptr()];
-    let shell_pid = spawn(shell_path, &shell_argv);
+    let result = std::process::Command::new("/shell").spawn().unwrap();
 
-    println!("[init] spawned shell with pid {}\n", shell_pid);
+    println!("[init] spawned shell with pid {}\n", result.id());
 }
