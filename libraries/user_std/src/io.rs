6fd795d95c92b54efe94d1570333b7f869ba23fc
Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>
2023-12-26 15:00:39 +0800

    User+Lib: added file io operations and changed `shell` to use std

--- libraries/user_std/src/io.rs
+++ libraries/user_std/src/io.rs
@@ -1,5 +1,8 @@
+use core::ffi::CStr;
+
 use kernel_user_link::call_syscall;
 use kernel_user_link::syscalls::SyscallError;
+use kernel_user_link::syscalls::SYS_OPEN;
 use kernel_user_link::syscalls::SYS_READ;
 use kernel_user_link::syscalls::SYS_WRITE;
 pub use kernel_user_link::FD_STDERR;
@@ -30,6 +33,24 @@ pub unsafe fn syscall_write(fd: usize, buf: &[u8]) -> Result<u64, SyscallError> 
             fd,                  // fd
             buf.as_ptr() as u64, // buf
             buf.len() as u64     // size
+        )
+    }
+}
+
+/// # Safety
+/// This function assumes that `path` is a valid C string.
+/// And that `access_mode` and `flags` are valid.
+pub unsafe fn syscall_open(
+    path: &CStr,
+    access_mode: usize,
+    flags: usize,
+) -> Result<u64, SyscallError> {
+    unsafe {
+        call_syscall!(
+            SYS_OPEN,
+            path.as_ptr() as u64, // path
+            access_mode as u64,   // access_mode
+            flags as u64          // flags
         )
     }
 }
--- userspace/shell/src/main.rs
+++ userspace/shell/src/main.rs
@@ -1,37 +1,8 @@
 #![feature(restricted_std)]
 #![no_main]
 
-use core::ffi::CStr;
-
-use kernel_user_link::{
-    call_syscall,
-    syscalls::{SYS_EXIT, SYS_OPEN, SYS_READ},
-};
-use std::string::String;
-
-fn open_file(path: &CStr) -> u64 {
-    unsafe {
-        call_syscall!(
-            SYS_OPEN,
-            path.as_ptr() as u64, // path
-            0,                    // flags
-            0                     // mode
-        )
-        .unwrap()
-    }
-}
-
-fn read_file(fd: u64, buf: &mut [u8]) -> u64 {
-    unsafe {
-        call_syscall!(
-            SYS_READ,
-            fd,                      // fd
-            buf.as_mut_ptr() as u64, // buf
-            buf.len() as u64         // size
-        )
-        .unwrap()
-    }
-}
+use kernel_user_link::{call_syscall, syscalls::SYS_EXIT};
+use std::{io::Read, string::String};
 
 fn exit(code: u64) -> ! {
     unsafe {
@@ -51,12 +22,11 @@ pub extern "C" fn _start() -> ! {
     println!("[shell] Hello!\n\n");
 
     // open `/message.txt` and print the result
-    let fd = open_file(c"/message.txt");
-    println!("[shell] content of `/message.txt`:\n");
-    let mut buf = [0u8; 1024];
-    let read = read_file(fd, &mut buf);
-    let data = String::from_utf8_lossy(&buf[..read as usize]);
 
-    println!("{}", data);
+    println!("[shell] content of `/message.txt`:\n");
+    let mut f = std::fs::File::open("/message.txt").unwrap();
+    let mut buf = [0; 100];
+    f.read(&mut buf).unwrap();
+    println!("{}", String::from_utf8_lossy(&buf));
     exit(222);
 }
