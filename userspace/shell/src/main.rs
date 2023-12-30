3a9623f186be204d376acc71307d370b3efafc09
Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>
2023-12-30 21:46:31 +0800

    User/Stdin: Changed the `shell` to use the new features of stdin
    Its a bit weird when you type you don't get your input in front of you before you type "enter".
    But that's fine, the user space API is good as is now.
    When we have terminals later, it will work better.

--- userspace/shell/src/main.rs
+++ userspace/shell/src/main.rs
@@ -1,6 +1,9 @@
 #![feature(restricted_std)]
 
-use std::{io::Read, string::String};
+use std::{
+    io::{self, Read},
+    string::String,
+};
 
 fn main() {
     // we are in `init` now
@@ -12,6 +15,15 @@ fn main() {
     println!("[shell] content of `/message.txt`:\n");
     let mut f = std::fs::File::open("/message.txt").unwrap();
     let mut buf = [0; 100];
-    f.read(&mut buf).unwrap();
-    println!("{}", String::from_utf8_lossy(&buf));
+    let res = f.read(&mut buf).unwrap();
+    println!("{}", String::from_utf8_lossy(&buf[..res]));
+
+    buf.fill(0);
+    println!("\n[shell] Input something:");
+    let res = io::stdin().read(&mut buf).unwrap();
+
+    println!(
+        "[shell] stdin: ({res}) {}",
+        String::from_utf8_lossy(&buf[..res])
+    );
 }
