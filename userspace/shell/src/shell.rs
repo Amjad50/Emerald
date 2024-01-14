55389a49ffe2b31a8fa98023b70a55bfc3974f2d
Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>
2024-01-14 09:40:28 +0800

    User/Shell: added `cd`, `pwd`, and `exit` commands
    Now we can control the current_dir, and play around with it.
    
    By default execution will try to execute binaries in the current path, otherwise it will use `/`, acting as PATH for now.

--- userspace/shell/src/shell.rs
+++ userspace/shell/src/shell.rs
@@ -1,10 +1,42 @@
 #![feature(restricted_std)]
 
 use std::{
+    borrow::Cow,
     io::{self, Write},
+    path::Path,
     process::Command,
     string::String,
 };
+
+/// Return `true` if we are the one handling this command, otherwise return `false`
+/// so that the command is executed as a normal process.
+fn handle_internal_cmds(cmd: &str, args: &[&str]) -> bool {
+    match cmd {
+        "exit" => {
+            println!("Goodbye!");
+            std::process::exit(0);
+        }
+        "cd" => {
+            if args.len() == 0 {
+                println!("cd: missing argument");
+            } else {
+                let path = args[0];
+                match std::env::set_current_dir(path) {
+                    Ok(_) => {}
+                    Err(e) => {
+                        println!("cd: {e}");
+                    }
+                }
+            }
+        }
+        "pwd" => {
+            println!("{}", std::env::current_dir().unwrap().display());
+        }
+        _ => return false,
+    }
+
+    true
+}
 
 fn main() {
     let mut old_result = None;
@@ -27,11 +59,22 @@ fn main() {
         }
 
         let cmd = args[0];
-        // TODO: add support for relative paths and other stuff, for now we must specify the full path
-        let cmd_path = format!("/{cmd}");
         let remaining_args = &args[1..];
 
-        let result = match Command::new(cmd_path).args(remaining_args).spawn() {
+        // handle internal commands
+        if handle_internal_cmds(cmd, remaining_args) {
+            continue;
+        }
+
+        // if this cmd exsist in the current directory, use it
+        // otherwise, use the root
+        let cmd_path: Cow<'_, str> = if Path::new(cmd).exists() {
+            cmd.into()
+        } else {
+            format!("/{}", cmd).into()
+        };
+
+        let result = match Command::new(cmd_path.as_ref()).args(remaining_args).spawn() {
             Ok(mut proc) => proc.wait().unwrap(),
             Err(e) => match e.kind() {
                 io::ErrorKind::NotFound => {
