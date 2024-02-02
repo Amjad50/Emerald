7ff2da58cd0f8d7c83890a2726f3bf5e90de0c6c
Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>
2024-02-02 19:16:06 +0800

    Docs: added memory_layout docs

--- .github/workflows/build_docs.sh
+++ .github/workflows/build_docs.sh
@@ -3,11 +3,11 @@
 set -eux pipefail
 
 BASE_DIR="$(git rev-parse --show-toplevel)"
-
+URL_ROOT_PATH="/"
 cd $BASE_DIR
 cargo doc
 (cd book
-    (cd src && sed -e "s#{ROOT_PATH}#${1-/}#g" links.md.template > links.md)
+    (cd src && sed -e "s#{ROOT_PATH}#${URL_ROOT_PATH}#g" links.md.template > links.md)
     mdbook build
     mkdir -p book/docs
     cp -r ../target/doc/* book/docs/
--- book/src/SUMMARY.md
+++ book/src/SUMMARY.md
@@ -2,6 +2,7 @@
 
 - [Kernel](./kernel/kernel.md)
     - [Boot](./kernel/boot.md)
+    - [Memory Layout](./kernel/memory_layout.md)
 - [Userspace](./userspace.md)
 - [Extra](./extra/index.md)
     - [Heap Allocator](./extra/heap_allocator.md)
