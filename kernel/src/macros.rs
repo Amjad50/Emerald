9fa875ac8efcb2d818ee047f4d2f08429c87084d
Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>
2024-02-17 09:13:03 +0800

    Kernel: added `dbg` macro
    This is useful for debugging. Taken from `std`

--- kernel/src/macros.rs
+++ kernel/src/macros.rs
@@ -25,3 +25,49 @@ macro_rules! eprintln {
     () => ($crate::eprint!("\n"));
     ($($arg:tt)*) => ($crate::eprint!("{}\n", format_args!($($arg)*)));
 }
+
+// copied from `std`
+#[macro_export]
+macro_rules! dbg {
+    // NOTE: We cannot use `concat!` to make a static string as a format argument
+    // of `eprintln!` because `file!` could contain a `{` or
+    // `$val` expression could be a block (`{ .. }`), in which case the `eprintln!`
+    // will be malformed.
+    () => {
+        $crate::println!("[{}:{}:{}]", ::core::file!(), ::core::line!(), ::core::column!())
+    };
+    ($val:expr $(,)?) => {
+        // Use of `match` here is intentional because it affects the lifetimes
+        // of temporaries - https://stackoverflow.com/a/48732525/1063961
+        match $val {
+            tmp => {
+                $crate::println!("[{}:{}:{}] {} = {:#?}",
+                    ::core::file!(), ::core::line!(), ::core::column!(), ::core::stringify!($val), &tmp);
+                tmp
+            }
+        }
+    };
+    ($($val:expr),+ $(,)?) => {
+        ($($crate::dbg!($val)),+,)
+    };
+}
+
+// same as above, but for eprintln!
+#[macro_export]
+macro_rules! edbg {
+    () => {
+        $crate::eprintln!("[{}:{}:{}]", ::core::file!(), ::core::line!(), ::core::column!())
+    };
+    ($val:expr $(,)?) => {
+        match $val {
+            tmp => {
+                $crate::eprintln!("[{}:{}:{}] {} = {:#?}",
+                    ::core::file!(), ::core::line!(), ::core::column!(), ::core::stringify!($val), &tmp);
+                tmp
+            }
+        }
+    };
+    ($($val:expr),+ $(,)?) => {
+        ($($crate::dbg!($val)),+,)
+    };
+}
