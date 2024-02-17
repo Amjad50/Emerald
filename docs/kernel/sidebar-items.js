f3e2880cc8978f5a7eddd44b1de2657aa1dd8b30
Amjad50 <Amjad50@users.noreply.github.com>
2024-02-17 02:57:44 +0000

    deploy: 9fa875ac8efcb2d818ee047f4d2f08429c87084d

--- docs/kernel/sidebar-items.js
+++ docs/kernel/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["finish_boot","kernel_main","load_init_process","panic"],"macro":["cpuid","eprint","eprintln","print","println"],"mod":["acpi","collections","cpu","devices","executable","fs","io","macros","memory_management","multiboot2","process","sync"]};
+window.SIDEBAR_ITEMS = {"fn":["finish_boot","kernel_main","load_init_process","panic"],"macro":["cpuid","dbg","edbg","eprint","eprintln","print","println"],"mod":["acpi","collections","cpu","devices","executable","fs","io","macros","memory_management","multiboot2","process","sync"]};
