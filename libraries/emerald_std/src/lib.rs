bb76d3aeac03cfc836db4bd2fd3c0a5854e73045
Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>
2024-02-23 20:10:46 +0800

    Lib+Extern: (temp) Added `weak` linking to math libraries
    This is clunky, as in `std`, we are using a relative path to
    `emerald_std`, and I don't really like that. But just to get this done,
    since the PRs are very slow.
    
    Here, we are using unstable feature to add all the math symbols, but
    with `weak` linking, so that the linker won't panic when it finds two of
    them defined (as some of them are already defined).

--- libraries/emerald_std/src/lib.rs
+++ libraries/emerald_std/src/lib.rs
@@ -1,4 +1,9 @@
 #![no_std]
+// used to weak link libm functions
+// this is temporary, waiting for PRs to be merged
+// into `compiler-builtins`
+// https://github.com/rust-lang/compiler-builtins/pull/577
+#![feature(linkage)]
 
 pub mod alloc;
 pub mod clock;
