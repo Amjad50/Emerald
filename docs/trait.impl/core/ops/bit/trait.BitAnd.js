423b8fa8aecdcdbcfe920508d86b5cdc06b775a9
Amjad50 <Amjad50@users.noreply.github.com>
2024-04-03 11:32:22 +0000

    deploy: 3b40c3dcb6c1cc16db752dd979e2534c4a605de6

--- docs/trait.impl/core/ops/bit/trait.BitAnd.js
+++ docs/trait.impl/core/ops/bit/trait.BitAnd.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {
+"emerald_kernel_user_link":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitAnd.html\" title=\"trait core::ops::bit::BitAnd\">BitAnd</a> for <a class=\"struct\" href=\"emerald_kernel_user_link/file/struct.OpenOptions.html\" title=\"struct emerald_kernel_user_link::file::OpenOptions\">OpenOptions</a>"]],
 "kernel":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitAnd.html\" title=\"trait core::ops::bit::BitAnd\">BitAnd</a> for <a class=\"struct\" href=\"kernel/fs/struct.FileAttributes.html\" title=\"struct kernel::fs::FileAttributes\">FileAttributes</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ docs/trait.impl/core/ops/bit/trait.BitAndAssign.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"emerald_kernel_user_link":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitAndAssign.html\" title=\"trait core::ops::bit::BitAndAssign\">BitAndAssign</a> for <a class=\"struct\" href=\"emerald_kernel_user_link/file/struct.OpenOptions.html\" title=\"struct emerald_kernel_user_link::file::OpenOptions\">OpenOptions</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
