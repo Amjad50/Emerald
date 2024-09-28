d943dd6a07295efac7e2f46772aeae99f0fad6a6
Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>
2024-09-28 15:12:07 +0800

    User+Lib: Switched to `power` command and added `reboot` handling
    Now we can shutdown and reboot with `power shutdown` and `power reboot`.
    
    It would be nice to have one command for each, but it would be more
    executable. A better approach is to support aliases.
    
    Signed-off-by: Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>

--- libraries/emerald_runtime/src/power.rs
+++ libraries/emerald_runtime/src/power.rs
@@ -3,15 +3,34 @@ use std::{
     io::{Error, Write},
 };
 
-use kernel_user_link::power::{POWER_DEVICE_PATH, SHUTDOWN_COMMAND};
+use kernel_user_link::power::{POWER_DEVICE_PATH, REBOOT_COMMAND, SHUTDOWN_COMMAND};
 
-pub fn shutdown() -> Result<(), Error> {
-    let mut power_file = File::options().write(true).open(POWER_DEVICE_PATH)?;
-    if power_file.write(SHUTDOWN_COMMAND)? != SHUTDOWN_COMMAND.len() {
-        return Err(Error::new(
-            std::io::ErrorKind::Other,
-            "failed to write all bytes",
-        ));
+pub enum PowerCommand {
+    Shutdown,
+    Reboot,
+}
+
+impl PowerCommand {
+    pub fn from_str(s: &str) -> Option<Self> {
+        match s {
+            "shutdown" => Some(Self::Shutdown),
+            "reboot" => Some(Self::Reboot),
+            _ => None,
+        }
     }
-    Ok(())
+
+    pub fn run(&self) -> Result<(), Error> {
+        let cmd = match self {
+            Self::Shutdown => SHUTDOWN_COMMAND,
+            Self::Reboot => REBOOT_COMMAND,
+        };
+        let mut power_file = File::options().write(true).open(POWER_DEVICE_PATH)?;
+        if power_file.write(cmd)? != cmd.len() {
+            return Err(Error::new(
+                std::io::ErrorKind::Other,
+                "failed to write all bytes",
+            ));
+        }
+        Ok(())
+    }
 }
--- libraries/kernel_user_link/src/power.rs
+++ libraries/kernel_user_link/src/power.rs
@@ -1,2 +1,3 @@
 pub const POWER_DEVICE_PATH: &str = "/devices/power";
 pub const SHUTDOWN_COMMAND: &[u8] = b"shutdown";
+pub const REBOOT_COMMAND: &[u8] = b"reboot";
